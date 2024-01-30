847ee3b03087cbf7e0cbc8a2df50ca0fb7e26b80
Jakub Dąbrowski <jakubdabrowski5701@gmail.com>
2024-01-30 13:00:02 +0100

    icons, new pages

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,42 +1,58 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Nav = () => {
-    const state = useSelector(state => state.handleCart)
-    return (
-        <nav className="navbar">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                 <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">YourFavYTber's merch</NavLink>
-                <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  const state = useSelector(state => state.handleCart);
 
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">
+          YourFavYTber's merch
+        </NavLink>
+        <button
+          className="navbar-toggler mx-2"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/product">
+                Products
+              </NavLink>
+            </li>
+          </ul>
 
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/">Home </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/product">Products</NavLink>
-                            </li>
-                      </ul>
+          <div className="buttons text-center">
+            <NavLink to="/login" className="btn btn-outline-dark m-2">
+              <i className="bi bi-person-fill-up mr-1"></i> Login
+            </NavLink>
+            <NavLink to="/register" className="btn btn-outline-dark m-2">
+              <i className="bi bi-person-plus-fill mr-1"></i> Register
+            </NavLink>
+            <NavLink to="/cart" className="btn btn-outline-dark m-2">
+              <i className="bi bi-cart3 mr-1"></i> Cart ({state.length})
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-                        <div className="buttons text-center">
-                        <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Login</NavLink>
-                        <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Register</NavLink>
-                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i> Cart ({state.length}) </NavLink>
-                    
-                        </div>
-
-                </div>
-            </div>
-            </nav>
-        </nav>
-        )
-    }
-    
-    export default Nav
+export default Nav;
