03b1ee081fe6ddf94b42eef9210eecac6b721bec
Jakub Dąbrowski <jakubdabrowski5701@gmail.com>
2024-02-01 01:04:59 +0100

    login fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -71,11 +71,11 @@ const Login = () => {
       <div className="container my-3 py-3">
         <h1 className="text-center">Login</h1>
         <hr />
-        <div class="row my-4 h-100">
+        <div className="row my-4 h-100">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
             <form onSubmit={handleSubmit}>
-              <div class="my-3">
-                <label for="display-4">Email address</label>
+              <div className="my-3">
+              <label htmlFor="floatingInput">Email address</label>
                 <input
                   type="email"
                   className={`form-control ${errors.email && "is-invalid"}`}
@@ -83,8 +83,8 @@ const Login = () => {
                   onChange={handleChange}
                 />
               </div>
-              <div class="my-3">
-                <label for="floatingPassword display-4">Password</label>
+              <div className="my-3">
+              <label htmlFor="floatingPassword display-4">Password</label>
                 <input
                   type="password"
                   className={`form-control ${errors.password && "is-invalid"}`}
@@ -96,7 +96,7 @@ const Login = () => {
                 <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
               </div>
               <div className="text-center">
-                <button class="my-2 mx-auto btn btn-dark" type="submit" >
+                <button className="my-2 mx-auto btn btn-dark" type="submit" >
                   Login
                 </button>
               </div>
